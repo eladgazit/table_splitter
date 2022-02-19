611314e01b6c19e2059a71bd62d54601e8d66eb0
elad.gazit <elad.gazit@playtika.com>
2022-02-19 11:08:54 +0200

    fix seed path refactor - clean code (named argument, type annotations etc..)

--- src/main/scala/data/TableSplitter.scala
+++ src/main/scala/data/TableSplitter.scala
@@ -5,23 +5,22 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions.col
 class TableSplitter extends AppLogger {
 
-  val eventColumnName = "event_name"
   val dbName = "mixedtiles"
+  val eventColumnName = "event_name"
+  val seedPath = "src/main/resources/seed"
 
   def main(args: Seq[String]): Unit = {
 
     val spark: Option[SparkSession] = Option(getSparkSession)
     dropDb(dbName, spark)
     createDb(dbName, spark)
-    val eventsDF: DataFrame = readDF(spark)
-    val distinctEventsDF: DataFrame = eventsDF.select(eventColumnName).distinct()
-    val distinctEvents: Array[Row] = distinctEventsDF.collect()
+    val eventsDF: DataFrame = readDF(spark, seedPath)
+    val distinctEvents: Array[Row] = eventsDF.select(eventColumnName).distinct().collect()
     splitAndWrite(spark, eventsDF, distinctEvents)
-    printAllTables(spark, eventsDF, distinctEvents)
-
+    printAllTables(spark, distinctEvents)
   }
 
-  private def splitAndWrite(spark: Option[SparkSession], eventsDF: DataFrame, distinctEvents: Array[Row]) = {
+  private def splitAndWrite(spark: Option[SparkSession], eventsDF: DataFrame, distinctEvents: Array[Row]): Unit = {
     distinctEvents.foreach {
       event: Row =>
         val eventName: String = event.get(0).asInstanceOf[String]
@@ -37,34 +36,32 @@ class TableSplitter extends AppLogger {
     }
   }
 
-  private def readDF(spark: Option[SparkSession]) = {
-    spark.get.read.json("/Users/Elad.Gazit/IdeaProjects/table_splitter/src/main/resources/seed")
+  private def readDF(spark: Option[SparkSession], path: String): DataFrame = {
+    spark.get.read.json(path)
   }
 
-  private def createDb(dbName: String, spark: Option[SparkSession]) = {
+  private def createDb(dbName: String, spark: Option[SparkSession]): DataFrame = {
     logger.info(s"create database if not exists $dbName")
     spark.get.sql(s"create database if not exists $dbName")
   }
 
-  private def dropDb(dbName: String, spark: Option[SparkSession]) = {
+  private def dropDb(dbName: String, spark: Option[SparkSession]): DataFrame = {
     logger.info(s"drop database if exists $dbName cascade")
     spark.get.sql(s"drop database if exists $dbName cascade")
   }
 
-  private def printAllTables(spark: Option[SparkSession], eventsDF: DataFrame, distinctEvents: Array[Row]) = {
+  private def printAllTables(spark: Option[SparkSession], distinctEvents: Array[Row]) = {
     logger.info(s"show tables in ${dbName}")
-    spark.get.sql(s"show tables in ${dbName}").show(false)
+    spark.get.sql(s"show tables in ${dbName}").show(truncate = false)
     distinctEvents.foreach {
       event: Row =>
         val eventName: String = event.get(0).asInstanceOf[String]
         val eventNameRenamed = renameEvent(eventName)
         val tableName = s"${dbName}.${eventNameRenamed}"
         logger.info(s"select * from ${tableName}")
-        spark.get.sql(s"select * from ${tableName}").show(false)
+        spark.get.sql(sqlText = s"select * from ${tableName}").show(truncate = false)
     }
   }
-
-
 
   /**
    * since table names can not contain white space or dashes but event name do, it is suggested to:
@@ -73,11 +70,11 @@ class TableSplitter extends AppLogger {
    * @param eventName
    * @return
    */
-  private def renameEvent(eventName: String) = {
+  private def renameEvent(eventName: String): String = {
     eventName.replace(" ", "_").replace("-", "_")
   }
 
-  def tableExists(table: String, spark: Option[SparkSession]) =
+  def tableExists(table: String, spark: Option[SparkSession]): Boolean =
     spark.get.catalog.tableExists(table)
 
   def getSparkSession: SparkSession =
